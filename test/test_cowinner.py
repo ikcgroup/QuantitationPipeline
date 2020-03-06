d96a0f581e1334376b7f579d97c8cb1011940f8f
Daniel Spencer <dms305@hku.hk>
2020-03-06 12:17:33 +0800

    Fix cowinner tests
    The original cowinner C# code contains a bug due to an off-by-one error.
    The tests are now benchmarked against a manually corrected output file.

--- test/test_cowinner.py
+++ test/test_cowinner.py
@@ -13,8 +13,12 @@ from quantify_proteins.utilities import read_tsv
 BENCHMARK_DIR = os.path.join("test_data", "6hr data", "cowinner")
 TEST_CONFIG_FILE = "test_config.json"
 RESULTS_DIR = os.path.join("test_data", "results", "cowinner")
+# Use the _FIXED file since the original was produced by the C# program
+# with an off-by-one error which resulted in the first line of the original
+# merge result not being considered on the second round of deduplication
 MERGE_BENCHMARK_FILE = os.path.join(
-    BENCHMARK_DIR, "merge", "20130512_nonphos_ProteinSummary_113 Merge.txt")
+    BENCHMARK_DIR, "merge",
+    "20130512_nonphos_ProteinSummary_113 Merge_FIXED.txt")
 
 
 class CoWinnerTests(unittest.TestCase):
@@ -49,6 +53,13 @@ class CoWinnerTests(unittest.TestCase):
         co_winner.merge()
 
         merged_df = read_tsv(os.path.join(co_winner._merged_dir, "merged.csv"))
+
+        # Rename "Unnamed" columns due to inserting a new column during
+        # processing
+        merged_df.rename(columns={"Unnamed: 10": "Unnamed: 11",
+                                  "Unnamed: 11": "Unnamed: 12"},
+                         inplace=True)
+
         benchmark_df = read_tsv(MERGE_BENCHMARK_FILE)
 
         pd.testing.assert_frame_equal(merged_df, benchmark_df)
